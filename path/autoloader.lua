510fa91e45569b274c9359f28787c9b96f207ede
orst777 <77461655+orst777@users.noreply.github.com>
2021-01-15 22:12:44 +0200

    Update autoloader.lua

--- path/autoloader.lua
+++ path/autoloader.lua
@@ -1,3 +1,5 @@
---os.autoloader({load: (os.execute('wget -f ..autoloader.. client/autoload'))})
+
+os.execute('/home/libs/AdapterHelper')
 os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/AdapterHelper/AdapterHelper.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G /home/libs/AdapterHelper/AdapterHelper.lua')
+os.execute('/home/libs/ShopUser')
 os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopUser/ShopUser.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G /home/libs/ShopUser/ShopUser.lua')
