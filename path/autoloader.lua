8e361d315feb57b553e174c9c74a6f02ff45efdc
orst777 <77461655+orst777@users.noreply.github.com>
2021-01-15 22:22:25 +0200

    Update autoloader.lua

--- path/autoloader.lua
+++ path/autoloader.lua
@@ -1,5 +1,5 @@
 
-os.execute('/home/libs/AdapterHelper')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/AdapterHelper/AdapterHelper.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G /home/libs/AdapterHelper/AdapterHelper.lua')
-os.execute('/home/libs/ShopUser')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopUser/ShopUser.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G /home/libs/ShopUser/ShopUser.lua')
+os.execute('mkdir /home/libs/AdapterHelper')
+os.execute('wget -f 'https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/AdapterHelper/AdapterHelper.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G' /home/libs/AdapterHelper/AdapterHelper.lua')
+os.execute('mkdir /home/libs/ShopUser')
+os.execute('wget -f 'https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopUser/ShopUser.lua?token=ASO7RFY7BOKAUCPK6NFKF63ABHW5G' /home/libs/ShopUser/ShopUser.lua')
