a5973dd1e2f0960813b0569480b5688945396b05
orst777 <77461655+orst777@users.noreply.github.com>
2021-01-28 17:46:57 +0200

    Update autoloader.lua

--- path/autoloader.lua
+++ path/autoloader.lua
@@ -2,22 +2,22 @@
 os.execute('rm -r /home/libs')
 
 os.execute('mkdir /home/libs/AdapterHelper')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/AdapterHelper/AdapterHelper.lua?token=ASO7RFYRDGSF3KVCXELQDJ3ABRNGY /home/libs/AdapterHelper/AdapterHelper.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/AdapterHelper/AdapterHelper.lua?token=ASO7RF46TMO6R5ACPHRDDGTADQLAQ /home/libs/AdapterHelper/AdapterHelper.lua')
 
 os.execute('mkdir /home/libs/LuaHelper')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/LuaHelper/LuaHelper.lua?token=ASO7RF47IDJYYXMQUR4FTRDABRNIK /home/libs/LuaHelper/LuaHelper.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/LuaHelper/LuaHelper.lua?token=ASO7RF6WWEXOBJDLZINNDWTADQLB6 /home/libs/LuaHelper/LuaHelper.lua')
 
 os.execute('mkdir /home/libs/ShopProxy')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopProxy/ShopItem.lua?token=ASO7RFZ3S7DKSN4ZTLSOV5LABRNO4 /home/libs/ShopProxy/ShopItem.lua')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopProxy/ShopProxy.lua?token=ASO7RF2EL55CALSP734FFP3ABRNTO /home/libs/ShopProxy/ShopProxy.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopProxy/ShopItem.lua?token=ASO7RF3R4H7PXPIOWX7SREDADQLFM /home/libs/ShopProxy/ShopItem.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopProxy/ShopProxy.lua?token=ASO7RF5HAKIW33LNKM3DMU3ADQLHG /home/libs/ShopProxy/ShopProxy.lua')
 
 os.execute('mkdir /home/libs/ShopUser')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopUser/ShopUser.lua?token=ASO7RF4IITY6CHJWH5CVXW3ABRNWK /home/libs/ShopUser/ShopUser.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ShopUser/ShopUser.lua?token=ASO7RF2YZ34E2KAQQ7PUUZDADQLJA /home/libs/ShopUser/ShopUser.lua')
 
 os.execute('mkdir /home/libs/ApiCommunicator')
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ApiCommunicator/ApiCommunicator.lua?token=ASO7RF75JSLD5L2D6JSU37DACCCGG /home/libs/ApiCommunicator/ApiCommunicator.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/ApiCommunicator/ApiCommunicator.lua?token=ASO7RF4MSFZ55QFQLBFSS6DADQLME /home/libs/ApiCommunicator/ApiCommunicator.lua')
 
 
 --Other Libs
-os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/json.lua?token=ASO7RF4SS5IROSXO5NUVZZTACCBGY /home/libs/json.lua')
+os.execute('wget -f https://raw.githubusercontent.com/SanyaRamzik/cw_quickShopClient/main/libs/json.lua?token=ASO7RF2VX6CCE2VDA26FYJDADQLOO /home/libs/json.lua')
 
