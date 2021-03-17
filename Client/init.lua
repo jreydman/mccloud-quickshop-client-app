836f222bdc17aefb841ede2af5964455c4977f0f
Ivan <proshkin.play.cannel@gmail.com>
2021-03-17 13:10:57 +0100

    Updated logo.lua

--- Client/init.lua
+++
@@ -1,6 +0,0 @@
-shell = require("shell")
-
---Set working path to libraries
-shell.setWorkingDirectory("/home/QShopClient")
-shell.setPath(shell.getPath()..":/libs")
-shell.setPath(shell.getPath()..":/bin")
